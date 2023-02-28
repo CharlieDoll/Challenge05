893d478a608de64ac4ebf758137a1a53a29d6d3f
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2023-02-28 21:07:01 +0000

    saves to local storage

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-$(document).ready(function () {
-  $(".saveButton").on("click", function () {
-    // need to find what the actual class is in our html that is associated with the input blocks
-    var text = $(this).sibling(".insideofinput").val();
-    var clock = $(this).parent().attr("id");
+var saveButton = $(".saveBtn");
 
-    localStorage.setItem(clock, text);
+$(document).ready(function () {
+  const inputArray = $("textarea");
+
+  if (localStorage.notes) {
+    const localStorageArray = JSON.parse(localStorage.notes);
+
+    for (let i = 0; i < inputArray.length; i++) {
+      inputArray[i].value = localStorageArray[i];
+    }
+  }
+
+  saveButton.on("click", function () {
+    // need to find what the actual class is in our html that is associated with the input blocks
+    const inputValues = [];
+
+    for (let i = 0; i < inputArray.length; i++) {
+      inputValues.push(inputArray[i].value);
+    }
+    localStorage.setItem("notes", JSON.stringify(inputValues));
   });
 
   function timeUpdate() {
@@ -19,25 +33,24 @@ $(document).ready(function () {
   setInterval(timeUpdate, 15000);
 
   // do this for every unique block
-  $("#blockId1 .insideofInput").val(localStorage.getItem("blockId1"));
-  $("#blockId2 .insideofInput").val(localStorage.getItem("blockId2"));
-  $("#blockId3 .insideofInput").val(localStorage.getItem("blockId3"));
-  $("#blockId4 .insideofInput").val(localStorage.getItem("blockId4"));
-  $("#blockId5 .insideofInput").val(localStorage.getItem("blockId5"));
-  $("#blockId6 .insideofInput").val(localStorage.getItem("blockId6"));
-  $("#blockId7 .insideofInput").val(localStorage.getItem("blockId7"));
-  $("#blockId8 .insideofInput").val(localStorage.getItem("blockId8"));
-  $("#blockId9 .insideofInput").val(localStorage.getItem("blockId9"));
-  $("#blockId10 .insideofInput").val(localStorage.getItem("blockId10"));
-  $("#blockId11 .insideofInput").val(localStorage.getItem("blockId11"));
-  $("#blockId12 .insideofInput").val(localStorage.getItem("blockId12"));
-  $("#blockId13 .insideofInput").val(localStorage.getItem("blockId13"));
-  $("#blockId14 .insideofInput").val(localStorage.getItem("blockId14"));
-  $("#blockId15 .insideofInput").val(localStorage.getItem("blockId15"));
-  $("#blockId16 .insideofInput").val(localStorage.getItem("blockId16"));
-  $("#blockId17 .insideofInput").val(localStorage.getItem("blockId17"));
-  $("#blockId18 .insideofInput").val(localStorage.getItem("blockId18"));
-  $("#blockId19 .insideofInput").val(localStorage.getItem("blockId19"));
-  $("#blockId20 .insideofInput").val(localStorage.getItem("blockId20"));
+
+  console.log(inputArray);
+
   $("#currentDay").text(moment().format("DD/MM/YYYY"));
 });
+
+var noteFormEl = $("#note-form");
+var noteDisplayEl = $("#note-display");
+
+// Gets project data from local storage and displays it
+function printNoteData() {
+  // clear current projects on the page
+  noteDisplayEl.empty();
+
+  // get projects from localStorage
+  var notes = readNotesFromStorage();
+}
+
+// var notes = readNotesFromStorage();
+// notes.push(newNote);
+// saveNotesToStorage(notes);
